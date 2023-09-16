801091af204191f41ed5c1d77f96dab9d29f6d79
Muzammildevs <muzammil.devqsols@gmail.com>
2023-09-16 09:24:15 -0700

    add count down on dashbaord top bar

--- src/components/shared/TopBar.js
+++ src/components/shared/TopBar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import TopCard from "./TopCard";
 import { useSelector } from "react-redux";
 
@@ -6,6 +6,54 @@ const TopBar = () => {
   const {
     user: { user }
   } = useSelector((state) => state.user);
+
+// Add CountDown Logic
+const [remainingDays, setRemainingDays] = useState(0);
+const [remainingHours, setRemainingHours] = useState(0);
+const [remainingMinutes, setRemainingMinutes] = useState(0);
+const [remainingSeconds, setRemainingSeconds] = useState(0);
+const [expired, setExpired] = useState(false);
+
+// Set the expiryDate with a valid date string
+const expiryDate = "2023-09-19";
+
+useEffect(() => {
+  const updateCountdown = () => {
+    const currentTime = new Date();
+    const remainingTimeMillis = new Date(expiryDate) - currentTime;
+
+    if (remainingTimeMillis <= 0) {
+      setExpired(true);
+      clearInterval(countdownInterval);
+      return;
+    }
+
+    const days = Math.floor(remainingTimeMillis / (1000 * 3600 * 24));
+    const hours = Math.floor((remainingTimeMillis % (1000 * 3600 * 24)) / (1000 * 3600));
+    const minutes = Math.floor((remainingTimeMillis % (1000 * 3600)) / (1000 * 60));
+    const seconds = Math.floor((remainingTimeMillis % (1000 * 60)) / 1000);
+
+    setRemainingDays(days);
+    setRemainingHours(hours);
+    setRemainingMinutes(minutes);
+    setRemainingSeconds(seconds);
+  };
+
+  // Initial call to update countdown
+  updateCountdown();
+
+  // Update the countdown every second
+  const countdownInterval = setInterval(updateCountdown, 1000);
+
+  // Cleanup the interval on unmount
+  return () => {
+    clearInterval(countdownInterval);
+  };
+}, [expiryDate]);
+
+console.log({ remainingDays }, { remainingHours }, { remainingMinutes }, { remainingSeconds });
+
+
   return (
     <div className="flex gap-4 flex-col md:flex-row justify-between">
       <TopCard
@@ -13,7 +61,7 @@ const TopBar = () => {
         caption={user?.purchased_plan?.name || "N/A"}
       />
       <TopCard title={"User Name"} caption={user.name} />
-      <TopCard title={"Expires In"} caption={"00:00:00:00"} />
+      <TopCard title={"Expires In"} caption={`${ remainingDays }:${ remainingHours }:${ remainingMinutes } :${ remainingSeconds }`} />
     </div>
   );
 };
