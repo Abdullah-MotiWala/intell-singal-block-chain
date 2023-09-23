2f20d7e37855293901428fd2cad894901c4fd8e8
Abdullah-MotiWala <motiwalaabdullah100@gmail.com>
2023-09-23 18:01:36 +0500

    ui responsiveness all componeted

--- src/components/shared/TopCard.js
+++ src/components/shared/TopCard.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const TopCard = ({ title, caption }) => {
   return (
-    <div className="panel-primary-bg h-24 w-full md:w-1/3 p-4 rounded flex flex-col items-center my-2">
+    <div className="panel-primary-bg h-24 p-4 rounded flex flex-col items-center my-2 w-[250px] md:w-full">
       <p className="font-light text-white text-lg ">{title}</p>
       <p className="font-bold text-white text-md">{caption}</p>
     </div>
